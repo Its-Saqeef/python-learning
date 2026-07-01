8fd69cf239b9b539e51ff10dc822c142c73fc3a1
its-saqeef <muhammadsaqeef@gmail.com>
2026-07-02 00:42:55 +0500

    Added number guessing game

---
+++ number_guessing_game.py
@@ -0,0 +1,20 @@
+import random
+
+print("Welcome to the Number Guessing Game!\n")
+start_point = int(input("Enter starting number : "))
+end_point = int(input("Enter ending number : "))
+
+number = random.randint(start_point,end_point)
+print(number)
+
+print(f"Select a number between {start_point} and {end_point} : ")
+
+while True:
+    user_guess = int(input("Enter number : "))
+    if user_guess > number:
+        print("Guess lower, try again!")
+    elif user_guess < number:
+        print("Guess higher, try again!")
+    else:
+        print("Bingo! Correct Answer")
+        break
